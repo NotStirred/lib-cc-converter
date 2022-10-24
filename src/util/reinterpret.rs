a4fad7ea451a873324302939a64809a95aca4706
Tom Martin <tom.martin1239@gmail.com>
2022-10-24 10:11:49 +0100

    util: Add Vec<u8> <-> Vec<i8> reinterpret functions

---
+++ src/util/reinterpret.rs
@@ -0,0 +1,33 @@
+pub fn vec_u8_into_i8(v: Vec<u8>) -> Vec<i8> {
+    // ideally we'd use Vec::into_raw_parts, but it's unstable,
+    // so we have to do it manually:
+
+    // first, make sure v's destructor doesn't free the data
+    // it thinks it owns when it goes out of scope
+    let mut v = std::mem::ManuallyDrop::new(v);
+
+    // then, pick apart the existing Vec
+    let p = v.as_mut_ptr();
+    let len = v.len();
+    let cap = v.capacity();
+
+    // finally, adopt the data into a new Vec
+    unsafe { Vec::from_raw_parts(p as *mut i8, len, cap) }
+}
+
+pub fn vec_i8_into_u8(v: Vec<i8>) -> Vec<u8> {
+    // ideally we'd use Vec::into_raw_parts, but it's unstable,
+    // so we have to do it manually:
+
+    // first, make sure v's destructor doesn't free the data
+    // it thinks it owns when it goes out of scope
+    let mut v = std::mem::ManuallyDrop::new(v);
+
+    // then, pick apart the existing Vec
+    let p = v.as_mut_ptr();
+    let len = v.len();
+    let cap = v.capacity();
+
+    // finally, adopt the data into a new Vec
+    unsafe { Vec::from_raw_parts(p as *mut u8, len, cap) }
+}
