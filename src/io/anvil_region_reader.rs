16bcfe728621b52e6bbd0b0e92efba704f18971f
Tom Martin <tom.martin1239@gmail.com>
2022-10-25 01:50:36 +0100

    io: anvil: Don't panic if invalid region or region entry is found

--- src/io/anvil_region_reader.rs
+++ src/io/anvil_region_reader.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
 use std::fs;
 use std::path::{Path, PathBuf};
 
@@ -13,6 +13,43 @@ pub struct AnvilRegionReader {
     region_location: PathBuf,
 }
 
+pub enum AnvilReadError {
+    StdIo(std::io::Error),
+    MissingHeader(RegionPos2d),
+}
+
+impl From<std::io::Error> for AnvilReadError {
+    fn from(err: std::io::Error) -> Self {
+        AnvilReadError::StdIo(err)
+    }
+}
+
+impl Debug for AnvilReadError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AnvilReadError::StdIo(err) => {
+                f.write_str(&*format!("Error when reading anvil region: {}", err))
+            }
+            AnvilReadError::MissingHeader(region_pos) => {
+                f.write_str(&*format!("Missing header in region {}", region_pos))
+            }
+        }
+    }
+}
+
+impl Display for AnvilReadError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AnvilReadError::StdIo(err) => {
+                f.write_str(&*format!("Error when reading anvil region: {}", err))
+            }
+            AnvilReadError::MissingHeader(region_pos) => {
+                f.write_str(&*format!("Missing header in region {}", region_pos))
+            }
+        }
+    }
+}
+
 impl AnvilRegionReader {
     pub const SECTOR_SIZE: usize = 4096;
     pub const SIZE_BITS: u32 = 8;
@@ -24,11 +61,15 @@ impl AnvilRegionReader {
         }
     }
 
-    pub fn read(&self, region_pos: RegionPos2d) -> Result<AnvilRegionData, Box<dyn Error>> {
+    pub fn read(&self, region_pos: RegionPos2d) -> Result<AnvilRegionData, AnvilReadError> {
         let region_key = region_pos.region_key();
         let region_path = self.region_location.join(region_key);
 
         let bytes = fs::read(region_path)?;
+        if bytes.len() < Self::SECTOR_SIZE {
+            return Err(AnvilReadError::MissingHeader(region_pos));
+        }
+
         let mut data_indices = Vec::with_capacity(RegionPos2d::CHUNKS_COUNT);
         for chunk_idx in 0..RegionPos2d::CHUNKS_COUNT {
             let sector_offset = chunk_idx * 4;
@@ -50,6 +91,11 @@ impl AnvilRegionReader {
 
             let slice_start = byte_offset + 4;
             let slice_end = byte_offset + (size * Self::SECTOR_SIZE);
+
+            // if header entry is invalid, set the entry to None
+            if slice_start > bytes.len() || slice_end > bytes.len() {
+                data_indices.push(None);
+            }
 
             data_indices.push(Some((slice_start, slice_end)));
         }
