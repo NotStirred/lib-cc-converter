27dede857806e8c7526a47c18715514a9df5aaad
Tom Martin <tom.martin1239@gmail.com>
2023-02-02 07:43:40 +0000

    convert: Add generic convert function. Add required supertraits to Reader/Converter/Writer traits

--- src/convert/converter.rs
+++ src/convert/converter.rs
@@ -1,4 +1,7 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    sync::{mpsc::TrySendError, Arc},
+};
 
 use crate::util::errors::error_from;
 
@@ -9,7 +12,7 @@ pub enum ReadError {
 }
 error_from!(ReadError, std::io::Error, ReadError::StdIo);
 
-pub trait Reader<K, IN> {
+pub trait Reader<K, IN>: Send {
     fn load_all_chunks<F>(&mut self, f: F) -> Result<(), ReadError>
     where
         F: Fn(IN);
@@ -22,7 +25,7 @@ pub enum WriteError {
 }
 error_from!(WriteError, std::io::Error, WriteError::StdIo);
 
-pub trait Writer<OUT> {
+pub trait Writer<OUT>: Send {
     fn write(&mut self, out_data: OUT) -> Result<(), WriteError>;
     fn flush(&mut self) -> Result<(), WriteError>;
 }
@@ -31,6 +34,84 @@ pub trait Writer<OUT> {
 pub enum ConversionError {
     Custom(Box<dyn Error>),
 }
-pub trait Converter<IN, OUT> {
+pub trait Converter<IN, OUT>: Send + Sync {
     fn convert(&self, in_data: IN) -> Result<Vec<OUT>, ConversionError>;
+}
+
+pub fn run_conversion<KEY, IN, OUT, READ, CONVERT, WRITE>(mut reader: READ, converter: CONVERT, mut writer: WRITE)
+where
+    IN: Send + 'static,
+    OUT: Send + 'static + Clone,
+    READ: Reader<KEY, IN> + 'static,
+    CONVERT: Converter<IN, OUT> + 'static,
+    WRITE: Writer<OUT> + 'static,
+{
+    let converter = Arc::new(converter);
+
+    let (convert_sender, convert_receiver) = multiqueue::mpmc_queue(64);
+    let (write_sender, write_receiver) = multiqueue::mpmc_queue(64);
+
+    let read_thread = std::thread::spawn(move || {
+        println!("Read thread start");
+        reader
+            .load_all_chunks(|mut data| loop {
+                match convert_sender.try_send(data) {
+                    Ok(_) => break,
+                    Err(err) => match err {
+                        TrySendError::Full(returned_data) => {
+                            data = returned_data;
+                            std::thread::yield_now()
+                        }
+                        _ => break,
+                    },
+                };
+            })
+            .unwrap();
+        println!("Read thread end");
+    });
+
+    let mut convert_threads = Vec::new();
+    for _ in 0..16 {
+        let converter = converter.clone();
+        let convert_receiver = convert_receiver.clone();
+        let write_sender = write_sender.clone();
+        convert_threads.push(std::thread::spawn(move || {
+            println!("Convert thread start");
+            while let Ok(data) = convert_receiver.recv() {
+                let converted = converter.convert(data).unwrap();
+
+                for mut data in converted.into_iter() {
+                    while let Err(err) = write_sender.try_send(data) {
+                        match err {
+                            TrySendError::Full(returned_data) => {
+                                data = returned_data;
+                                std::thread::yield_now()
+                            }
+                            _ => break,
+                        }
+                    }
+                }
+            }
+
+            println!("Convert thread end");
+        }));
+    }
+    convert_receiver.unsubscribe();
+    write_sender.unsubscribe();
+
+    let write_thread = std::thread::spawn(move || {
+        println!("Write thread start");
+
+        while let Ok(data) = write_receiver.recv() {
+            writer.write(data).unwrap();
+        }
+        writer.flush().unwrap();
+        println!("Write thread end");
+    });
+
+    read_thread.join().unwrap();
+    for thread in convert_threads {
+        thread.join().unwrap()
+    }
+    write_thread.join().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,10 @@ pub mod util;
 
 #[cfg(test)]
 mod tests {
-    use std::sync::mpsc::TrySendError;
-    use std::sync::Arc;
 
     use crate::convert::anvil2cc::Anvil2CCConverter;
 
-    use crate::convert::converter::{Converter, Reader, Writer};
+    use crate::convert::converter::run_conversion;
     use crate::io::anvil_region_reader::AnvilRegionReader;
     use crate::io::cubic_chunks_writer::CubicChunksWriter;
 
@@ -20,74 +18,10 @@ mod tests {
         let src_path = test_utils::test_resources_path().join("simple_region_test");
         let dst_path = test_utils::test_resources_path().join("simple_region_test/out");
 
-        let mut reader = AnvilRegionReader::new(&src_path);
-        let converter = Arc::new(Anvil2CCConverter::new(true));
-        let mut writer = CubicChunksWriter::new(&dst_path, 64).unwrap();
-        let (convert_sender, convert_receiver) = multiqueue::mpmc_queue(64);
-        let (write_sender, write_receiver) = multiqueue::mpmc_queue(64);
+        let reader = AnvilRegionReader::new(&src_path);
+        let converter = Anvil2CCConverter::new(true);
+        let writer = CubicChunksWriter::new(&dst_path, 64).unwrap();
 
-        let read_thread = std::thread::spawn(move || {
-            println!("Read thread start");
-            reader
-                .load_all_chunks(|mut data| loop {
-                    match convert_sender.try_send(data) {
-                        Ok(_) => break,
-                        Err(err) => match err {
-                            TrySendError::Full(returned_data) => {
-                                data = returned_data;
-                                std::thread::yield_now()
-                            }
-                            _ => break,
-                        },
-                    };
-                })
-                .unwrap();
-            println!("Read thread end");
-        });
-
-        let mut convert_threads = Vec::new();
-        for _ in 0..16 {
-            let converter = converter.clone();
-            let convert_receiver = convert_receiver.clone();
-            let write_sender = write_sender.clone();
-            convert_threads.push(std::thread::spawn(move || {
-                println!("Convert thread start");
-                while let Ok(data) = convert_receiver.recv() {
-                    let converted = converter.convert(data).unwrap();
-
-                    for mut data in converted.into_iter() {
-                        while let Err(err) = write_sender.try_send(data) {
-                            match err {
-                                TrySendError::Full(returned_data) => {
-                                    data = returned_data;
-                                    std::thread::yield_now()
-                                }
-                                _ => break,
-                            }
-                        }
-                    }
-                }
-
-                println!("Convert thread end");
-            }));
-        }
-        convert_receiver.unsubscribe();
-        write_sender.unsubscribe();
-
-        let write_thread = std::thread::spawn(move || {
-            println!("Write thread start");
-
-            while let Ok(data) = write_receiver.recv() {
-                writer.write(data).unwrap();
-            }
-            writer.flush().unwrap();
-            println!("Write thread end");
-        });
-
-        read_thread.join().unwrap();
-        for thread in convert_threads {
-            thread.join().unwrap()
-        }
-        write_thread.join().unwrap();
+        run_conversion(reader, converter, writer);
     }
 }
